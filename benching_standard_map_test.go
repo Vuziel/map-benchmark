b3417f5b8753fd7fc536d5459b297d59c1689f5a
ikhakimov <ikhakimov@gw.list.goodwix.ru>
2024-01-16 07:46:22 +0300

    table test for standard map

--- benching_standard_map_test.go
+++ benching_standard_map_test.go
@@ -1,138 +1,109 @@
 package bench
 
 import (
+	"fmt"
 	"testing"
 )
-
-// todo -> table tests
 
 // todo -> https://github.com/orcaman/concurrent-map
 
 // todo - hash table исполь., метод цепочек
 
-func insertXStandardMap(x int, b *testing.B) {
-	testMap := make(map[int]int, x)
-	b.ResetTimer()
-	for i := 0; i < x; i++ {
+func BenchmarkInsertStandardMap(b *testing.B) {
+	benchmarks := []struct {
+		elems int
+		mP    map[int]int
+	}{
+		{elems: 100},
+		{elems: 1_000},
+		{elems: 10_000},
+		{elems: 100_000},
+		{elems: 1_000_000},
+	}
+
+	for _, bench := range benchmarks {
+		bench.mP = make(map[int]int, bench.elems)
+		b.ResetTimer()
+
+		b.Run(fmt.Sprintf("input map with amount of elements %d", bench.elems), func(t *testing.B) {
+			for i := 0; i < b.N; i++ {
+				insertStandardMap(bench.elems, bench.mP)
+			}
+		})
+	}
+}
+
+func insertStandardMap(elems int, testMap map[int]int) {
+	for i := 0; i < elems; i++ {
 		testMap[i] = i
 	}
 }
 
-func BenchmarkInsertStandardMap_1_000_000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		insertXStandardMap(1_000_000, b)
+func BenchmarkSelectStandardMap(b *testing.B) {
+	benchmarks := []struct {
+		elems int
+		mP    map[int]int
+	}{
+		{elems: 100},
+		{elems: 1_000},
+		{elems: 10_000},
+		{elems: 100_000},
+		{elems: 1_000_000},
+	}
+
+	for _, bench := range benchmarks {
+		bench.mP = make(map[int]int, bench.elems)
+		for i := 0; i < bench.elems; i++ {
+			bench.mP[i] = i
+		}
+		b.ResetTimer()
+
+		b.Run(fmt.Sprintf("map with amount of elements %d", bench.elems), func(t *testing.B) {
+			for i := 0; i < b.N; i++ {
+				selectStandardMap(bench.elems, bench.mP)
+			}
+		})
 	}
 }
 
-func BenchmarkInsertStandardMap_100_000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		insertXStandardMap(100_000, b)
+func selectStandardMap(elems int, testMap map[int]int) {
+	for i := 0; i < elems; i++ {
+		j := testMap[i]
+
+		if j != 0 {
+		}
 	}
 }
 
-func BenchmarkInsertStandardMap_10_000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		insertXStandardMap(10_000, b)
+func BenchmarkDeleteStandardMap(b *testing.B) {
+	benchmarks := []struct {
+		elems int
+		mP    map[int]int
+	}{
+		{elems: 100},
+		{elems: 1_000},
+		{elems: 10_000},
+		{elems: 100_000},
+		{elems: 1_000_000},
+	}
+
+	for _, bench := range benchmarks {
+		bench.mP = make(map[int]int, bench.elems)
+		for i := 0; i < bench.elems; i++ {
+			bench.mP[i] = i
+		}
+		b.ResetTimer()
+
+		b.Run(fmt.Sprintf("map with amount of elements %d", bench.elems), func(t *testing.B) {
+			for i := 0; i < b.N; i++ {
+				deleteStandardMap(bench.elems, bench.mP)
+			}
+		})
 	}
 }
 
-func BenchmarkInsertStandardMap_1000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		insertXStandardMap(1000, b)
-	}
-}
-
-func BenchmarkInsertStandardMap_100(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		insertXStandardMap(100, b)
-	}
-}
-
-func selectXStandardMap(x int, b *testing.B) {
-	testMap := make(map[int]int, x)
-	for i := 0; i < x; i++ {
-		testMap[i] = i
-	}
-	var holder int
-	b.ResetTimer()
-
-	for i := 0; i < x; i++ {
-		holder = testMap[i]
-	}
-
-	if holder != 0 {
-	}
-}
-
-func BenchmarkSelectStandardMap_1_000_000(b *testing.B) {
-	// todo map create, reset time
-	for i := 0; i < b.N; i++ {
-		selectXStandardMap(1_000_000, b)
-	}
-}
-
-func BenchmarkSelectStandardMap_100_000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		selectXStandardMap(100_000, b)
-	}
-}
-
-func BenchmarkSelectStandardMap_10_000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		selectXStandardMap(10_000, b)
-	}
-}
-
-func BenchmarkSelectStandardMap_1000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		selectXStandardMap(1000, b)
-	}
-}
-
-func BenchmarkSelectStandardMap_100(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		selectXStandardMap(100, b)
-	}
-}
-
-func deleteXStandardMap(x int, b *testing.B) {
-	testMap := make(map[int]int, x)
-	for i := 0; i < x; i++ {
-		testMap[i] = i
-	}
-	b.ResetTimer()
-
+func deleteStandardMap(x int, testMap map[int]int) {
 	for i := 0; i < x; i++ {
 		delete(testMap, i)
-	}
-}
-
-func BenchmarkDeleteStandardMap_1_000_000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		deleteXStandardMap(1_000_000, b)
-	}
-}
-
-func BenchmarkDeleteStandardMap_100_000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		selectXStandardMap(100_000, b)
-	}
-}
-
-func BenchmarkDeleteStandardMap_10_000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		selectXStandardMap(10_000, b)
-	}
-}
-
-func BenchmarkDeleteStandardMap_1000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		selectXStandardMap(1000, b)
-	}
-}
-
-func BenchmarkDeleteStandardMap_100(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		selectXStandardMap(100, b)
 	}
 }
